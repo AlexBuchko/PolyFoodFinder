d565d737b8de78f25d0140790637fc708cbf02e6
AlexBuchko <abuchko@calpoly.edu>
2022-11-04 18:36:50 -0700

    Add database functionality + backend route to update foods

--- model/food-services.js
+++ model/food-services.js
@@ -83,7 +83,24 @@ async function findFoodById(id) {
     }
 }
 
+async function updateOne(id, update) {
+    /* updates the food at the given id with the passed in update object
+    update: {field: value} can have 1+ field - value pairs
+    return: the updated document
+    EX: updateOne(<id>, {likes: 5}) would return the food at that ID with it's likes set to 5
+    */
+    const foodsModel = getDbConnection().model("foods", foodSchema);
+    try {
+        //new: true makes it return the updated food, not the old one
+        return await foodsModel.findByIdAndUpdate(id, update, { new: true });
+    } catch (err) {
+        console.log("error in updateOne", err);
+        return undefined;
+    }
+}
+
 exports.getFoods = getFoods;
 exports.findFoodById = findFoodById;
 exports.getFoodsByFilters = getFoodsByFilters;
 exports.getDbConnection = getDbConnection;
+exports.updateOne = updateOne;

f077b673db7da882e18cbdc13dfa2869bcc024e4
AlexBuchko <abuchko@calpoly.edu>
2022-11-14 15:05:13 -0800

    Adding testing framework

---
+++ testing/models.test.js
@@ -0,0 +1,8 @@
+const foodServices = require("../model/food-services");
+const resServices = require("../model/food-services");
+
+describe("foodServices", () => {
+    test("first test", () => {
+        expect(true).toBe(true);
+    });
+});
